c793e6646d265379b872188055fe375499e18c5d
Cookie Jar <jdechant11@gmail.com>
2020-10-05 19:59:01 -0500

    Created JavaScript pseudocode and an html heading.

---
+++ quiz.js
@@ -0,0 +1,16 @@
+// Start with an array object for questions.
+
+
+// Create a start button using Bootstrap.
+
+
+// Add a timer - look back to Speed Reader exercise.
+
+
+// Use of on-click, event listeners, and form features.
+
+
+// I will need a for loop for questions.
+
+
+// Review how to save information to the browser?
